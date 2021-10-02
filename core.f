044975cf62cab766eaf4970aa39f89d5e39e5b72
James Nelson <jamestn529@gmail.com>
2021-10-02 03:04:34 -0600

    larger comment headings, and give credit in core.f

--- core.f
+++ core.f
@@ -1,15 +1,22 @@
+\
 \ Constants
+\
 
 :true 1 ;
 :false 0 ;
 
+\
 \ Stack operators
+\
 
 :2drop  drop drop ;
 :swap over >r nip r> ;
 :rot >r swap r> swap ;
 
+\
 \ Combinators
+\
+\ largely from https://gist.github.com/crcx/8060687#file-combinators-forth
 
 :dip swap >r call r> ;
 :sip over >r call r> ;
@@ -18,7 +25,9 @@
 :bi@ dup bi* ;
 :tri swap >r [ sip ] dip sip r> call swap ;
 
+\
 \ Control flow
+\
 
 :?swap  ?exit swap ;
 
