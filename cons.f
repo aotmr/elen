f451c1e26eaf0981f9a3490d9370b93019b1b8a4
James Nelson <jamestn529@gmail.com>
2021-10-02 11:07:06 -0600

    add alignment helper function to cons

--- cons.f
+++ cons.f
@@ -1,4 +1,9 @@
-:cons here >r 2, r> ;
+\ for our pointer tags to work, all cons cells
+\ must be allocated aligned to even addresses
+:cons-align here 1 and 0= ?exit 0 , ;
+
+:cons cons-align here >r 2, r> ;
+
 :car @ ;
 :cdr 1 + @ ;
 :carcdr 2@ ;
@@ -6,3 +11,6 @@
 :null? 0= ;
 :atom? [ 1 and 0<> ] &null? bi or ;
 :pair? atom? 0= ;
+
+:>atom 2* 1+ ;
+:atom> 1- 2/ ;
